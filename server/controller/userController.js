1b7dcfab72168fedc3e42d05a19c81542cc8aefe
edoyang <edoyangz@gmail.com>
2024-12-08 21:54:10 +1100

    leave strict

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -37,7 +37,7 @@ exports.loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "None",
       maxAge: 60 * 60 * 1000 * 24,
     });
 
